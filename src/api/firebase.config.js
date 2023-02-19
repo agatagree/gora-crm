f28909a9307005107c0d07a66fd487a2c7499263
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2023-02-19 16:47:51 +0100

    basic setup

---
+++ src/api/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
+  appId: "1:636764236912:web:ae47ee5af3323a58be18b5",
+  projectId: "gora0-8bdf5",
+};
---
+++ src/api/firebase.js
@@ -0,0 +1,13 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { firebaseConfig } from "./firebase.config";
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+
